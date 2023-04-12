2d7848cd3ab1c973c1d41c78e404fca53e2ef2b4
matiss <matissjurevics@gmail.com>
2023-04-12 14:10:22 +0100

    idk anymore

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,38 @@
 <script>
-	const converToInput = () => {
-		console.log("Clicked")
-	}
+	// @ts-nocheck
+	import Input from "../components/input.svelte"
+
 	$: content = [
 		"## Title",
 		"Lorem ipsum, `dolor sit amet consectetur adipisicing elit.` Numquam ea eius expedita optio fugiat accusamus id. Fugiat quia laborum totam, laudantium beatae natus. Modi nihil suscipit labore consectetur, cupiditate numquam architecto unde obcaecati similique exercitationem officia, corrupti perferendis repudiandae debitis sunt ducimus laudantium amet adipisci necessitatibus est deleniti! Nesciunt, rerum.",
 		"## Sub-title"
 	] 
-	let inputText = ""
-	const enterDetect = (e) => {
-		if (e.key == "Enter") {
-			content.push(inputText)
-			content = content
-			inputText = ""
-		}
-	}
-	const turnToInput = (e) => {
-		let element = e.target
-		let index = content.indexOf(e.target.firstChild.data)
-		console.log(index)
-		e.target.classList.add("hidden")
-		console.log(e.target.parentNode)
-	}
+	let notes = ""
+	
+	
+	
 	
 </script>
 
-<nav ></nav>
+<nav class="navbar ">
+	<div class="navbar-start">left</div>
+	<div class="navbar-center">middle</div>
+</nav>
 
 <main class="flex flex-row justify-center align-middle w-full h-screen bg-accent/5">
 	<div class="bg-white min-w-96 w-1/2 min-h-[1000px] h-2/3 mt-auto rounded-2xl drop-shadow-xl p-8 flex flex-col space-y-3" >
-		{#each content as item}
-			<span on:click={e => turnToInput(e)} on:keydown={() => {console.log("hi")}}>{item}</span>
-		{/each}
-		<textarea name="" id="" cols="30" rows="5" class="w-full h-32 p-3 text-xl font-extralight rounded-md border-2 border-black/5 focus:outline-none focus:border-secondary/30 focus:border-2 resize-none" bind:value={inputText} on:keypress={e => enterDetect(e)}></textarea>
+		<textarea name="" id="" cols="30" rows="5" class="w-full h-full p-3 text-xl font-extralight rounded-md border-2 border-black/5 focus:outline-none focus:border-secondary/30 focus:border-2 resize-none" bind:value={notes}></textarea>
 	</div>
 </main>
 
-<ul class="absolute bottom-5 right-5">
-	<li class="h-16 w-16 rounded-full border-[2px] border-secondary bg-white flex justify-center align-middle">h1</li>
-	<li></li>
-</ul>
+<div class="absolute bottom-5 right-5">
+	<div class="card w-96 bg-base-100 shadow-xl">
+		<div class="card-body">
+		  <h2 class="card-title text-secondary">AI Assistant</h2>
+		  <p class="text-lg">How can I help you today?</p>
+		  <input type="text" class="w-full h-12 mt-4 p-3 text-lg font-extralight rounded-md border-2 border-black/5 focus:outline-none focus:border-secondary/30 focus:border-2 resize-none">
+		</div>
+	  </div>
+</div>
 
 
